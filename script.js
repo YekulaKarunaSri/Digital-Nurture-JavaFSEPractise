73246066c3ffea9cd89cbebc1910f53a7ea333b5
Karuna Sri <karunasri1115@gmail.com>
2026-06-04 15:27:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function submit() {
+  {
+  let branch = document.getElementById("branch").value;
+  if (branch == "cse") {
+    console.log("computer science and engineering");
+  }
+  return false;
+}
+}
+functon changetheme()
+document.body.sty
+}
